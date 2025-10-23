ee1b9f9888bcdccbf133ec8c0d7bdaa28c3a732b
arkytlee <dobidugi@gmail.com>
2025-10-23 17:43:26 +0900

    fix: application_start.sh ( add permission )

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -8,4 +8,4 @@ echo "Starting Spring Boot application..."
 # nohup: 터미널이 끊겨도 계속 실행 (백그라운드)
 # 2>&1: 에러 로그(stderr)를 일반 로그(stdout)와 함께 app.log 파일에 저장
 # &: 백그라운드에서 실행
-nohup java -jar $JAR_PATH  --server.port=80 > $LOG_PATH 2>&1 &
+nohup sudo java -jar $JAR_PATH  --server.port=80 > $LOG_PATH 2>&1 &
