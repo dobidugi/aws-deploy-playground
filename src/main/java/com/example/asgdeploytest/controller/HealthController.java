1844289fac2fc43e0922e6429f2bc8aaea7c9a0f
arkytlee <dobidugi@gmail.com>
2025-10-23 17:52:00 +0900

    fix: health api response

--- src/main/java/com/example/asgdeploytest/controller/HealthController.java
+++ src/main/java/com/example/asgdeploytest/controller/HealthController.java
@@ -1,14 +1,20 @@
 package com.example.asgdeploytest.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HealthController {
 
     @GetMapping("/health")
-    ResponseEntity<Void> health() {
-        return ResponseEntity.ok().build();
+    ResponseEntity<Map<String, String>> health() {
+        Map<String, String> map = new HashMap<>();
+        map.put("status", "UP");
+        return ResponseEntity.status(HttpStatus.OK).body(map);
     }
 }
